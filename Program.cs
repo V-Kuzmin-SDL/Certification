805816f65208c805eaade58df69fedd481e986da
V-Kuzmin-SDL <161224746+V-Kuzmin-SDL@users.noreply.github.com>
2024-05-07 19:03:27 +0300

    Add data to Readme

--- Program.cs
+++ Program.cs
@@ -20,5 +20,19 @@ foreach (var str in initialArray)
     }
 }
 
+string[] resultArray = new string[count];
+int index = 0;
+foreach (var str in initialArray)
+{
+    if (str.Length <= 3)
+    {
+        resultArray[index++] = str;
+    }
+}
+Console.WriteLine("Новый массив строк, длина которых меньше или равна 3 символам:");
+foreach (var str in resultArray)
+{
+    Console.WriteLine(str);
+}
 
 
