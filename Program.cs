4b31ef4570137490b60fb549614bcb1f15254479
V-Kuzmin-SDL <161224746+V-Kuzmin-SDL@users.noreply.github.com>
2024-05-07 18:35:23 +0300

    Add data to programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// программа, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+
+string[] initialArray = { "Sunny", "17", "Sol", ":-)" };
+Console.WriteLine("Первоначальный массив строк:");
+foreach (var str in initialArray)
+{
+    Console.WriteLine(str);
+}
+
+int count = 0;
+foreach (var str in initialArray)
+{
+    if (str.Length <= 3)
+    {
+        count++;
+    }
+}
+
+
+
