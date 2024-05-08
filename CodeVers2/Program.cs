728a756ace45f668f039db4e4d819597144e5250
V-Kuzmin-SDL <161224746+V-Kuzmin-SDL@users.noreply.github.com>
2024-05-08 20:33:36 +0300

    Add data to programm Code Version 2

--- CodeVers2/Program.cs
+++ CodeVers2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+    
+        string[] initialArray = { "Russia", "China", "Sol", ":-)" };
+        Console.WriteLine("Первоначальный массив:");
+        PrintArray(initialArray);
+
+        string[] resultArray = FilterArray(initialArray);
+        Console.WriteLine("Новый массив, длина строк меньше или равна 3 символам:");
+        PrintArray(resultArray);
+    
+
+    static string[] FilterArray(string[] array)
+    {
+        int count = GetCountOfShortStrings(array);
+        string[] resultArray = new string[count];
+
+        int index = 0;
+        foreach (var str in array)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index++] = str;
+            }
+        }
+
+        return resultArray;
+    }
+
+    static int GetCountOfShortStrings(string[] array)
+    {
+        int count = 0;
+        foreach (var str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void PrintArray(string[] array)
+    {
+        foreach (var str in array)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+
