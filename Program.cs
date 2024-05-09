8352b2fd9917e83cfa636b64a327a72a5ce3dc22
V-Kuzmin-SDL <161224746+V-Kuzmin-SDL@users.noreply.github.com>
2024-05-09 14:49:51 +0300

    Fix problem2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] initialArray = { "hello", "2", "world", ":-)" };
+
+// Выводим первоначальный массив на экран
+Console.WriteLine("Первоначальный массив строк:");
+foreach (var str in initialArray)
+{
+Console.WriteLine(str);
+}
+
+// Подсчитываем, сколько строк удовлетворяют условию
+int count = 0;
+foreach (var str in initialArray)
+{
+if (str.Length <= 3)
+{
+count++;
+}
+}
+
+// Создаем новый массив для строк, удовлетворяющих условию
+string[] resultArray = new string[count];
+
+// Заполняем новый массив подходящими строками
+int index = 0;
+foreach (var str in initialArray)
+{
+if (str.Length <= 3)
+{
+resultArray[index++] = str;
+}
+}
+
+// Выводим результат на экран
+Console.WriteLine("Новый массив строк, длина которых меньше или равна 3 символам:");
+foreach (var str in resultArray)
+{
+Console.WriteLine(str);
+}
+
